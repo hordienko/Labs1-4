e4a213ed7541d3ee017697a6d3a6681eb098425e
Anna Hordiienko <annahordienko@yahoo.com>
2021-03-29 19:12:58 +0300

    commit

---
+++ 3 лаба/BranchAndBound-Algorithm-master/BranchAndBound-Algorithm-master/TSP1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"1. Отримати матрицю з файлу");
+           // Console.WriteLine($"2. Випадкові дані");
+           //Console.WriteLine($"3. Тест");
+            var decision = Convert.ToInt32(Console.ReadLine());
+            var loader = new Loader();
+            switch (decision)
+            {
+                case 1:
+                    Console.WriteLine($"Назва файлу:");
+                    var name = Console.ReadLine();
+                    var data = loader.ReadFromFile($"{name}.txt");
+                    if (data.Cities == 0)
+                    {
+                        Console.ReadKey();
+                        return;
+                    }
+                    var solver = new BBAlgorithm(data);
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    solver.Solve();
+                    sw.Stop();
+                    Console.WriteLine(sw.Elapsed);
+                    break;
+
+                //case 2:
+                //    Console.WriteLine($"Podal liczbe miast: ");
+                //    var cities = Convert.ToInt32(Console.ReadLine());
+                //    var dataa = loader.RandomFile(cities);
+                //    var solver2 = new BBAlgorithm(dataa);
+                //    var sww = new Stopwatch();
+                //    sww.Start();
+                //    solver2.Solve();
+                //    sww.Stop();
+                //    Console.WriteLine(sww.Elapsed);
+                //    break;
+                //case 3:
+                //    var testArray = new[] {8, 10, 12, 14, 16, 18, 20};
+                    
+                //    foreach (var test in testArray)
+                //    {
+                //        var span = new TimeSpan();
+                //        using (var file = new StreamWriter($"test{test}.txt"))
+                //        {
+                //            var _sw = new Stopwatch();
+                //            for (var i = 0; i < 100; i++)
+                //            {
+                //                _sw.Reset();
+                //                var _data = loader.RandomFile(test);
+                //                var _solver = new BBAlgorithm(_data);
+                //                _sw.Start();
+                //                _solver.Solve();
+                //                _sw.Stop();
+                //                file.WriteLine(_sw.Elapsed);
+                //                span += _sw.Elapsed;
+                //            }
+                //            file.WriteLine($"srednia: {span.Duration()}");
+                //        }
+                //    }
+                //    break;
+
+                default:
+                    Console.WriteLine($"Натисни 1");
+                    break;
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 4 лаба/Max-Flow-Problem-master/ConsoleApp/Program.cs
@@ -0,0 +1,200 @@
+﻿using PathSearching;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ConsoleApp.Experiment;
+using Algorithms.Solvers;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            IOConsole console = new IOConsole();
+            while (true)
+            {
+                try
+                {
+
+                    Console.Clear();
+                    if (console.CMatrix == null)
+                        Console.WriteLine("*** Немає матрицi ***\n");
+                    else
+                        Console.WriteLine("*** Матриця готова ***\n");
+                    Console.WriteLine("1. Зчитати матрицю з файлу");
+                   // Console.WriteLine("2. Console input of matrix");
+                  //  Console.WriteLine("3. Random generating matrix");
+                    Console.WriteLine("2. Вивести матрицю ");
+                    Console.WriteLine("3. Вирiшення методом Форда-Фалкерсона");
+                   // Console.WriteLine("6. Dinics solution");
+                    //Console.WriteLine("7. Greedy search of path");
+                    //Console.WriteLine("8. DFS of path");
+                    //Console.WriteLine("9. Experimental research");
+                    Console.WriteLine("4. Вийти");
+                    var key = Console.ReadLine();
+                    Console.Clear();
+                    switch (key)
+                    {
+                        case "1":
+                            if (console.CMatrix != null)
+                            {
+                                Console.WriteLine("Матриця вже є! " +
+                                  "Перезаписати її? [y/n]");
+                                char k;
+                                do
+                                {
+                                    k = Console.ReadKey().KeyChar;
+                                } while (k != 'n' && k != 'y');
+                                if (k == 'n')
+                                    break;
+                            }
+
+                            Console.WriteLine("\nВвести назву файлу: ");
+                            var filename = Console.ReadLine();
+                            if (!File.Exists(filename))
+                            {
+                                Console.WriteLine("Файл з назвою  \"" + filename + "\" не iснує");
+                                Console.ReadKey();
+                                continue;
+                            }
+                            console = new IOConsole(filename);
+                            console.ReadMatrix();
+                            console.WriteMatrix(console.CMatrix);
+                            Console.ReadKey();
+                            break;
+                        //case "2":
+                        //    if (console.CMatrix != null)
+                        //    {
+                        //        Console.WriteLine("You have already got a matrix. " +
+                        //          "Do you really want to rewrite it? [y/n]");
+                        //        char k;
+                        //        do
+                        //        {
+                        //            k = Console.ReadKey().KeyChar;
+                        //        } while (k != 'n' && k != 'y');
+                        //        if (k == 'n')
+                        //            break;
+                        //    }
+
+                        //    console = new IOConsole();
+                        //    console.ConsoleInputMatrix();
+                        //    console.WriteMatrix(console.CMatrix);
+                        //    Console.ReadKey();
+                        //    break;
+                        //case "3":
+                        //    if (console.CMatrix != null)
+                        //    {
+                        //        Console.WriteLine("You have already got a matrix. " +
+                        //          "Do you really want to rewrite it? [y/n]");
+                        //        char k;
+                        //        do
+                        //        {
+                        //            k = Console.ReadKey().KeyChar;
+                        //        } while (k != 'n' && k != 'y');
+                        //        if (k == 'n')
+                        //            break;
+                        //    }
+
+                        //    console = new IOConsole();
+                        //    int bound = console.RandomInput();
+                        //    console.RandomGenerate(bound);
+                        //    console.WriteMatrix(console.CMatrix);
+                        //    Console.ReadKey();
+                        //    break;
+                        case "2":
+                            if (console.CMatrix == null)
+                                Console.WriteLine("Немає матрицi");
+                            else
+                                console.WriteMatrix(console.CMatrix);
+                            Console.ReadKey();
+                            break;
+                        case "3":
+                            if (console.CMatrix == null)
+                            {
+                                Console.WriteLine("Немає матрицi");
+                                Console.ReadKey();
+                                continue;
+                            }
+
+                            Console.WriteLine("Вирiшення методом Форда-Фалкерсона:");
+                            Solver solve = new Solver(console.From - 1, console.To - 1, console.N, console.CMatrix);
+                            console.WriteFlowMatrixToConsole(console.CMatrix, console.GetNullMatrix());
+                            solve.FordFulkerson();
+
+                            Console.WriteLine($"Результат: Max F = {solve.F}");
+                            console.WriteFlowMatrixToConsole(console.CMatrix, solve.FlowMatrix.FlowToIntMatrix());
+                            Console.ReadKey();
+                            break;
+                        //case "3":
+                        //    if (console.CMatrix == null)
+                        //    {
+                        //        Console.WriteLine("Немає матрицi");
+                        //        Console.ReadKey();
+                        //        continue;
+                        //    }
+
+                        //    Console.WriteLine("\nDinics solution:");
+                        //    Dinics dinics = new Dinics(console.From - 1, console.To - 1, console.N, console.CMatrix);
+                        //    console.WriteFlowMatrixToConsole(console.CMatrix, console.GetNullMatrix());
+                        //    dinics.Run();
+
+                        //    Console.WriteLine("\nResult: Max F = " + dinics.F.ToString());
+                        //    console.WriteFlowMatrixToConsole(console.CMatrix, dinics.FlowMatrix.FlowToIntMatrix());
+                        //    Console.ReadKey();
+                        //    break;
+                        //case "7":
+                        //    if (console.CMatrix == null)
+                        //    {
+                        //        Console.WriteLine("You haven`t got any matrix to use");
+                        //        Console.ReadKey();
+                        //        continue;
+                        //    }
+
+                        //    Console.WriteLine("\nGreedy solution:");
+                        //    var greedy = new Greedy(console.CMatrix, console.N, console.From - 1, console.To - 1);
+                        //    (int costGreedy, List<int> pathGreedy) = greedy.GreedyAlgorithm();
+                        //    console.WriteListToConsole(pathGreedy, costGreedy);
+                        //    Console.ReadKey();
+                        //    break;
+                        //case "8":
+                        //    if (console.CMatrix == null)
+                        //    {
+                        //        Console.WriteLine("You haven`t got any matrix to use");
+                        //        Console.ReadKey();
+                        //        continue;
+                        //    }
+
+                        //    Console.WriteLine("\nDFS solution:");
+                        //    var dfs = new DFS(console.CMatrix, console.N, console.From - 1, console.To - 1);
+                        //    (int costDFS, List<int> pathDFS) = dfs.Run();
+                        //    console.WriteListToConsole(pathDFS, costDFS);
+                        //    Console.ReadKey();
+                        //    break;
+                        //case "9":
+                        //    console = new IOConsole();
+                        //    Research experimentor = new Research();
+                        //    experimentor.Menu();
+                        //    break;
+                        case "4":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Неправильний вибiр");
+                            Console.ReadKey();
+                            break;
+                    }
+                }catch(StackOverflowException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadKey();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
